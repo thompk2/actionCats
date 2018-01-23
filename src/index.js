51cfa82752c1f6f15f7af797ae91cbbe92d0ab9f
Kathryn Iris Thompson <kthompson@toasttab.com>
2018-01-23 12:25:31 -0500

    remove code to push into PR

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { displayCatName } from './cat_generator';
-
-(() => {
-    const button = document.getElementById('cat-button');
-    const displayBlock = document.getElementById('cat-display');
-
-    button.addEventListener('click', () => displayCatName(displayBlock));
-})();
